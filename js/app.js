c388a2204a65c3281c529f0709914cb5ee70eb08
Arjun Dogra <66183261+arjundogra@users.noreply.github.com>
2020-07-03 21:25:39 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -38,49 +38,7 @@ function buildNav(){
     for (i=1; i<=4;i++){
         const list = document.createElement("li");
         const anchor = document.createElement("a");
-        const text = document.createTextNode("Section" + i);
-        anchor.className = "menu__link";
-        anchor.appendChild(text);
-        anchor.setAttribute('href', `#section`+i)
-        list.appendChild(anchor);
-        const navbar = document.querySelector("#navbar__list");
-        navbar.appendChild(list);
-    }
-}
 
-buildNav();    
-
-// Add class 'active' to section when near top of viewport
-function active(){
-	for (var i = 1; i <= 4; i++) {
-		var section = document.querySelector('#section'+i);
-		const box = section.getBoundingClientRect();
-		console.log(box);
-		if (box.top <= 150 && box.bottom >= 150) {
-        section.classList.add('your-active-class');
-        section.style.border = "2px solid blue";
-        } 
-        else{
-        	section.classList.remove('your-active-class');
-        	section.style.border = "";
-        }
-    }
-}
-
-document.addEventListener('scroll',function(){
-	active();
-});
-
-// Scroll to anchor ID
-const allAnchor = document.querySelectorAll("a");
-allAnchor.forEach(item => {
-	item.addEventListener("click", function(){
-	const moveTo = this.hash;
-    $('html').animate({
-        scrollTop: $(moveTo).position().top
-    });
-	})
-});	
 	
 
 /**
