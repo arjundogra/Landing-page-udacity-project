ca3bf1d482acf96e9a1f17f49736060e3e694821
Arjun Dogra <66183261+arjundogra@users.noreply.github.com>
2020-07-23 08:34:00 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -38,8 +38,56 @@ function buildNav(){
     for (i=1; i<=4;i++){
         const list = document.createElement("li");
         const anchor = document.createElement("a");
+        const text = document.createTextNode("Section" + i);
+        anchor.className = `menu__link section${i}`;
+        anchor.appendChild(text);
+        anchor.setAttribute('href', `#section`+i)
+        list.appendChild(anchor);
+        const navbar = document.querySelector("#navbar__list");
+        navbar.appendChild(list);
+    }
+}
 
-	
+buildNav();  
+
+// Scroll To the section
+document.querySelectorAll('.navbar__menu a').forEach(a => {
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}); 
+
+// Add class 'active' to section when near top of viewport
+function active(){
+	for (var i = 1; i <= 4; i++) {
+		var section = document.querySelector('#section'+i);
+		const box = section.getBoundingClientRect();
+		console.log(box);
+		if (box.top <= 150 && box.bottom >= 150) {
+            const activeSection = section.getAttribute("id");
+            console.log(activeSection);
+            document.querySelector(`.${activeSection}`).style.color = "red";
+            section.classList.add('your-active-class');
+            section.style.border = "2px solid white";
+        } 
+        else{
+            const activeSection = section.getAttribute("id");
+            document.querySelector(`.${activeSection}`).style.color = "black";
+        	section.classList.remove('your-active-class');
+        	section.style.border = "";
+        }
+    }
+}
+
+document.addEventListener('scroll',function(){
+	active();
+});
+
+// Scroll to anchor ID
+
 
 /**
  * End Main Functions
